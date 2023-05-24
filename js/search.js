cf8961d020f936a0cd393dd2eeb278036b69f074
ysnt123 <379152651@qq.com>
2023-05-24 14:56:33 +0800

    Site updated: 2023-05-24 14:56:33

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-// A local search script with the help of [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
+// A local search script with the help of hexo-generator-search
 // Copyright (C) 2015 
 // Joseph Pan <http://github.com/wzpan>
 // Shuhao Mao <http://github.com/maoshuhao>
@@ -18,104 +18,127 @@
 // 02110-1301 USA
 // 
 
-var searchFunc = function(path, search_id, content_id) {
+var searchFunc = function (path, search_id, content_id) {
     'use strict';
+    var BTN = "<i id='local-search-close'>x</i>";
     $.ajax({
         url: path,
         dataType: "xml",
-        success: function( xmlResponse ) {
+        success: function (xmlResponse) {
             // get the contents from search data
-            var datas = $( "entry", xmlResponse ).map(function() {
+            var datas = $("entry", xmlResponse).map(function () {
                 return {
-                    title: $( "title", this ).text(),
-                    content: $("content",this).text(),
-                    url: $( "url" , this).text()
+                    title: $("title", this).text(),
+                    content: $("content", this).text(),
+                    url: $("url", this).text()
                 };
             }).get();
 
             var $input = document.getElementById(search_id);
-			if (!$input) return;
             var $resultContent = document.getElementById(content_id);
-            if ($("#local-search-input").length > 0) {
-                $input.addEventListener('input', function () {
-                    var str = '<ul class=\"search-result-list\">';
-                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                    $resultContent.innerHTML = "";
-                    if (this.value.trim().length <= 0) {
-                        return;
+
+            $input.addEventListener('input', function () {
+                var str = '<ul class=\"search-result-list\">';
+                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                $resultContent.innerHTML = "";
+                if (this.value.trim().length <= 0) {
+                    return;
+                }
+                // perform local searching
+                datas.forEach(function (data) {
+                    var isMatch = true;
+                    var content_index = [];
+                    if (!data.title || data.title.trim() === '') {
+                        data.title = "Untitled";
                     }
-                    // perform local searching
-                    datas.forEach(function (data) {
-                        var isMatch = true;
-                        var content_index = [];
-                        if (!data.title || data.title.trim() === '') {
-                            data.title = "Untitled";
-                        }
-                        var data_title = data.title.trim().toLowerCase();
-                        var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-                        var data_url = data.url;
-                        var index_title = -1;
-                        var index_content = -1;
-                        var first_occur = -1;
-                        // only match artiles with not empty contents
-                        if (data_content !== '') {
-                            keywords.forEach(function (keyword, i) {
-                                index_title = data_title.indexOf(keyword);
-                                index_content = data_content.indexOf(keyword);
+                    var data_title = data.title.trim().toLowerCase();
+                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+                    var data_url = data.url;
+                    var index_title = -1;
+                    var index_content = -1;
+                    var first_occur = -1;
+                    // only match artiles with not empty contents
+                    if (data_content !== '') {
+                        keywords.forEach(function (keyword, i) {
+                            index_title = data_title.indexOf(keyword);
+                            index_content = data_content.indexOf(keyword);
 
-                                if (index_title < 0 && index_content < 0) {
-                                    isMatch = false;
-                                } else {
-                                    if (index_content < 0) {
-                                        index_content = 0;
-                                    }
-                                    if (i == 0) {
-                                        first_occur = index_content;
-                                    }
-                                    // content_index.push({index_content:index_content, keyword_len:keyword_len});
+                            if (index_title < 0 && index_content < 0) {
+                                isMatch = false;
+                            } else {
+                                if (index_content < 0) {
+                                    index_content = 0;
                                 }
-                            });
-                        } else {
-                            isMatch = false;
-                        }
-                        // show search results
-                        if (isMatch) {
-                            str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
-                            var content = data.content.trim().replace(/<[^>]+>/g, "");
-                            if (first_occur >= 0) {
-                                // cut out 100 characters
-                                var start = first_occur - 20;
-                                var end = first_occur + 80;
-
-                                if (start < 0) {
-                                    start = 0;
+                                if (i == 0) {
+                                    first_occur = index_content;
                                 }
-
-                                if (start == 0) {
-                                    end = 100;
-                                }
-
-                                if (end > content.length) {
-                                    end = content.length;
-                                }
-
-                                var match_content = content.substring(start, end);
-
-                                // highlight all keywords
-                                keywords.forEach(function (keyword) {
-                                    var regS = new RegExp(keyword, "gi");
-                                    match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
-                                });
-
-                                str += "<p class=\"search-result\">" + match_content + "...</p>"
+                                // content_index.push({index_content:index_content, keyword_len:keyword_len});
                             }
-                            str += "</li>";
+                        });
+                    } else {
+                        isMatch = false;
+                    }
+                    // show search results
+                    if (isMatch) {
+                        str += "<li><a href='" + data_url +
+                            "' class='search-result-title'>" + data_title + "</a>";
+                        var content = data.content.trim().replace(/<[^>]+>/g, "");
+                        if (first_occur >= 0) {
+                            // cut out 100 characters
+                            var start = first_occur - 20;
+                            var end = first_occur + 80;
+
+                            if (start < 0) {
+                                start = 0;
+                            }
+
+                            if (start == 0) {
+                                end = 100;
+                            }
+
+                            if (end > content.length) {
+                                end = content.length;
+                            }
+
+                            var match_content = content.substr(start, end);
+
+                            // highlight all keywords
+                            keywords.forEach(function (keyword) {
+                                var regS = new RegExp(keyword, "gi");
+                                match_content = match_content.replace(regS,
+                                    "<em class=\"search-keyword\">" +
+                                    keyword + "</em>");
+                            });
+
+                            str += "<p class=\"search-result\">" + match_content +
+                                "...</p>"
                         }
-                    });
-                    str += "</ul>";
-                    $resultContent.innerHTML = str;
+                        str += "</li>";
+                    }
                 });
-            }
+                str += "</ul>";
+                if (str.indexOf('<li>') === -1) {
+                    return $resultContent.innerHTML = BTN +
+                        "<ul><span class='local-search-empty'>没有找到内容，更换下搜索词试试吧~<span></ul>";
+                }
+                $resultContent.innerHTML = BTN + str;
+            });
         }
+    });
+    $(document).on('click', '#local-search-close', function () {
+        $('#local-search-input').val('');
+        $('#local-search-result').html('');
+    });
+    $(document).on('focus', '#local-search', function () {
+        $('#local-search-icon-search').html('❌');
+        $('#local-search-icon-search').attr('id', 'local-search-icon-close');
+        //console.log("66666");
+    });
+    $(document).on('click', '#local-search-icon-close', function () {
+        $('#local-search-input').val('');
+        $('#local-search-result').html('');
+        $('#local-search-icon-close').html('🔍');
+        $('#local-search-icon-close').attr('id', 'local-search-icon-search');
+        //console.log("1111");
     });
 }
